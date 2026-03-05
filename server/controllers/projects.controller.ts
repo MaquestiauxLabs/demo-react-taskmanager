9b105d82743da3067b009cbf439cd7fca6e122aa
Jean-Philippe Maquestiaux <maquejp@gmail.com>
2026-03-05 11:21:33 +0100

    PROJECTS: Add members for multi-user collaboration Fixes #62

--- server/controllers/projects.controller.ts
+++ server/controllers/projects.controller.ts
@@ -47,4 +47,18 @@ export class ProjectsController {
     const result = await this.service.unarchive(id as string);
     res.status(result.httpStatus).json(result);
   };
+
+  assignUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const result = await this.service.assignUser(id as string, userId);
+    res.status(result.httpStatus).json(result);
+  };
+
+  unassignUser = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const result = await this.service.unassignUser(id as string, userId);
+    res.status(result.httpStatus).json(result);
+  };
 }
